ecd812963a71f942bd5f348db348170020cfb633
Sâmara Tinoco <143561594+Sam-Tinocco@users.noreply.github.com>
2025-10-28 11:53:26 -0300

    Add files via upload

---
+++ CálculoEstatura.py
@@ -0,0 +1,11 @@
+'''ENUNCIADO: Construa um programa no qual um usuário informe a sua
+estatura em metros e o programa converta-a para centímetros.
+AUTORA: Sâmara Tinoco
+Data: 21/10/2025
+Fonte: 
+'''
+print ("\nInicio!")
+estatura = float(input("Digite a sua estatura (em metros): "))
+estatura = estatura * 100
+print (f"Sua estatura é de {estatura} cm.")
+print("Fim!")
