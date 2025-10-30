cffa71bf9e82654b0fccc57eaa9885216bf9bc9e
Sâmara Tinoco <143561594+Sa-Tinocco@users.noreply.github.com>
2025-10-30 08:05:24 -0300

    Add files via upload

---
+++ Leitura2NumerosReais.py
@@ -0,0 +1,13 @@
+'''ENUNCIADO: Construa um programa que leia 2 números reais informados 
+pelo usuário.
+AUTORA: Sâmara Tinoco
+DATA: 23/10/2025
+FONTE: EP_Exerc03Pag52.py
+'''
+print("\nInicio")
+vlrNro1 = int(input("Digite informando o primeiro número: "))
+vlrNro2 = int(input("Digite informando o segundo número: "))
+print(f"\nA soma dos números {vlrNro1} e {vlrNro2} é = {vlrNro1 + vlrNro2}")
+print(f"\nO produto dos números {vlrNro1} e {vlrNro2} é = {vlrNro1 * vlrNro2}")
+
+print("\nFim!")
